1f2bf3141ef37ddc7f0f9d1d2afcc64bd8daf3b0
Brian Oblad <a.gray.cloud@gmail.com>
2019-10-02 17:13:37 -0400

    test subdomain

--- server/index.js
+++ server/index.js
@@ -1,27 +1,47 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const path = require('path')
-const db = require('./db')
+const express = require('express');
+const subdomain = require('express-subdomain');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const db = require('./db');
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+const router = express.Router()
 
-app.get('/api/users', async (req, res, next) => {
+router.get('/' async (req, res, next) => {
   try {
-    const users = await db.models.user.findAll()
-    res.json(users)
-  } catch (e) {
-    console.error(e)
-    next(e)
+    res.send('subdomain get')
+  } catch (error) {
+    next(error)
   }
 })
 
+app.use(subdomain('foo', router))
 
-module.exports = app
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', async (req, res, next) => {
+  try {
+    res.send('foo');
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/users', async (req, res, next) => {
+  try {
+    const users = await db.models.user.findAll();
+    res.json(users);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+module.exports = app;
 
 if (require.main === module) {
-  app.listen(PORT, (e) => {
-    if (e) throw e
-    console.log(`listening on port ${PORT}`)
-  })
+  app.listen(PORT, e => {
+    if (e) throw e;
+    console.log(`listening on port ${PORT}`);
+  });
 }
